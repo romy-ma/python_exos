3c578e67e7eb31d175a080709ac52eb1d7124ab4
ROMY <mama.marouainfo11@gmail.com>
2024-12-12 18:42:48 +0100

    3rd exo

---
+++ exoDrei.py
@@ -0,0 +1,13 @@
+temperature = int(input("enter the temperature :"))
+
+if temperature <=0 :
+    print("It's freezing!")
+else:
+  if temperature <= 10 :
+     print("It's cold!")
+  elif temperature <= 20:
+        print("It's cool!")
+
+print("Stay safe!")
+
+
